f4f08e4ad809aa0ba60c1c92b35cbebe26e26958
Anurigwo Chibuzor <chibuzoraustine50@gmail.com>
2025-06-30 03:48:48 +0100

    Bug fix

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type UserOperation = {
+    sender: `0x${string}`;
+    nonce: number;
+    initCode: string;
+    callData: string;
+    callGasLimit: bigint;
+    verificationGasLimit: bigint;
+    preVerificationGas: bigint;
+    maxFeePerGas: bigint;
+    maxPriorityFeePerGas: bigint;
+    paymasterAndData: string;
+    signature?: string;
+  };
+  
